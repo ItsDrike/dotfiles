20c1734cbcf319258ad4a18d4b7eb15dc1b71e96
ItsDrike <itsdrikeofficial@gmail.com>
2021-07-14 14:24:08 +0200

    remove deploy source

--- install.sh
+++
@@ -1,9 +0,0 @@
-if ! type "python3" &> /dev/null; then
-	sudo pacman -S python
-fi
-if ! type "pip3" &> /dev/null; then
-	sudo pacman -S python-pip
-fi
-
-pip install -r requirements.txt
-python3 -m src
