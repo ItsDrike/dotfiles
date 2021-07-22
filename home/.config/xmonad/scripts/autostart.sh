4988a0b97067ae91578f46fe0b3b4cb881ef82c6
ItsDrike <itsdrikeofficial@gmail.com>
2021-07-22 22:21:27 +0200

    Add autostart script from ~/.config/autostart

---
+++ home/.config/xmonad/scripts/autostart.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Automatically start the applications in $HOME/.config/autostart
+
+AUTOSTART_DIR="${XDG_CONFIG_HOME:-$HOME/.config}/autostart"
+
+find $AUTOSTART_DIR -name '*.desktop' -exec ~/.local/bin/scripts/deskopen {} +
