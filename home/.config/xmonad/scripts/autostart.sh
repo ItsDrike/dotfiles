742f8f6382069d43f39541058fb775baf2d15e6e
ItsDrike <itsdrikeofficial@gmail.com>
2021-08-24 15:40:24 +0200

    Fix autostarting desktop apps with spaces

--- home/.config/xmonad/scripts/autostart.sh
+++ home/.config/xmonad/scripts/autostart.sh
@@ -3,5 +3,4 @@
 # Automatically start the applications in $HOME/.config/autostart
 
 AUTOSTART_DIR="${XDG_CONFIG_HOME:-$HOME/.config}/autostart"
-
-find $AUTOSTART_DIR -name '*.desktop' -exec ~/.local/bin/scripts/deskopen {} +
+find "$AUTOSTART_DIR" -name '*.desktop' -exec ~/.local/bin/scripts/deskopen "{}" \;
