a26a3c4c78a34ea91ab72091b2e7861a7c780a20
ItsDrike <itsdrikeofficial@gmail.com>
2021-09-02 17:38:48 +0200

    Fix autostart

--- home/.config/xmonad/scripts/autostart.sh
+++ home/.config/xmonad/scripts/autostart.sh
@@ -3,4 +3,5 @@
 # Automatically start the applications in $HOME/.config/autostart
 
 AUTOSTART_DIR="${XDG_CONFIG_HOME:-$HOME/.config}/autostart"
-find "$AUTOSTART_DIR" -name '*.desktop' -exec ~/.local/bin/scripts/deskopen "{}" \;
+
+find $AUTOSTART_DIR -name "*.desktop" | xargs -I {} sh -c "~/.local/bin/scripts/deskopen {} &"
