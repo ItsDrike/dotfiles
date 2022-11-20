b9128710700a07bdf2fe772b0670ba485e408398
ItsDrike <itsdrike@protonmail.com>
2022-11-20 03:07:41 +0100

    Remove everything and restart blank
    This commit removes all files currently present in the repo, to prepare
    for a start from a nothing. This is done due to my recent migration from
    X11 to Wayland, which has rendered most of these config files no longer
    releveant.
    
    I've currently been tracking my dotfiles in a separate repository, in
    hopes to get it to a state where it would be mergable here, but that
    turned out to be much more difficult than I anticipated, and I think it
    will be much easier to simply move over the history from this temporary
    repository I've been using onto this one. That however requires a start
    from a clean point, which this commit creates.

--- home/.config/xmonad/scripts/autostart.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Automatically start the applications in $HOME/.config/autostart
-
-AUTOSTART_DIR="${XDG_CONFIG_HOME:-$HOME/.config}/autostart"
-
-find $AUTOSTART_DIR -name "*.desktop" | xargs -I {} sh -c "~/.local/bin/scripts/deskopen {} &"
--- home/.local/scripts/uninstall_virtmic.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Uninstall the virtual microphone.
-
-pactl unload-module module-pipe-source
-rm $HOME/.config/pulse/client.conf
-
