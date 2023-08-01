e2ae926dbc7c0d75983d19ab7b339796f00469e6
ItsDrike <itsdrike@protonmail.com>
2023-08-01 14:19:35 +0200

    Add tood on potential git pull

--- install_gui.sh
+++ install_gui.sh
@@ -98,6 +98,7 @@ yay -S --noconfirm --needed \
 mkdir -p ~/.local/src
 git clone --recursive https://github.com/hyprwm/Hyprland ~/.local/src/Hyprland || true # don't fail if exists
 pushd ~/.local/src/Hyprland
+# TODO: Consider git pull here, in case the clone fell through and hyprland was already installed
 git checkout "$(git rev-list --tags --max-count=1)" # check out the last tag (latest stable release)
 sudo make install
 popd
