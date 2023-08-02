c4053d84965fd92de861a5bbaeb54c2ce5b6e1b5
ItsDrike <itsdrike@protonmail.com>
2023-08-02 02:24:15 +0200

    Use the git version of xdg-ninja

--- install_user.sh
+++ install_user.sh
@@ -38,7 +38,7 @@ yay -S --noconfirm --needed \
   openssh cronie exa bat dust mlocate lshw trash-cli ncdu btop \
   dnsutils net-tools wget jq fzf polkit rebuild-detector hyperfine mediainfo git-delta \
   python-pip ripgrep zip unzip usbutils hexyl strace python-poetry pyenv yt-dlp \
-  downgrade lf xdg-ninja
+  downgrade lf xdg-ninja-git
 
 # Copy over zsh configuration
 cp -a home/.zshenv ~
