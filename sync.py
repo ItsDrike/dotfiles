296a5b86cfa23d4d800fea2313fd3c9b2dd2cffc
ItsDrike <itsdrike@protonmail.com>
2023-10-10 20:52:35 +0200

    Don't show .zgenom files in diff

--- sync.py
+++ sync.py
@@ -372,6 +372,7 @@ def exclude_fun(diff: FileDiff) -> bool:
         lambda d: Path("root/usr/share/zsh/site-functions/zsh-you-should-use") in d.rel_dot_file.parents,
         lambda d: Path("root/usr/share/zsh/site-functions/zsh-syntax-highlighting") in d.rel_dot_file.parents,
         lambda d: Path("root/usr/share/zsh/site-functions/zsh-autosuggestions") in d.rel_dot_file.parents,
+        lambda d: Path("home/.config/zsh/.zgenom") in d.rel_dot_file.parents,
         lambda d: Path("home/.cache/zsh/history") == d.rel_dot_file and d.status is DiffStatus.CONTENT_DIFFERS,
         lambda d: Path("home/.config/nomacs/Image Lounge.conf") == d.rel_dot_file
         and d.status is DiffStatus.CONTENT_DIFFERS,
