b0ca57ac4ae56dcf138ca3250800b962317c5f60
ItsDrike <itsdrike@protonmail.com>
2024-10-13 21:56:51 +0200

    Add custom service to watch monitor updates

---
+++ home/.config/hyprland-monitord/added.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -euo pipefail
+
+MONITOR_ID="$1"
+MONITOR_NAME="$2"
+MONITOR_DESCRIPTION="$3"
+
+eww open bar1
---
+++ home/.config/hyprland-monitord/removed.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -euo pipefail
+
+MONITOR_NAME="$1"
+
+eww close bar1
