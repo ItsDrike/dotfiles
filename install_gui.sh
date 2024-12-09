31ab2cdb8edfd7602742ffec0cf547a8fba32677
ItsDrike <itsdrike@protonmail.com>
2024-12-09 18:12:25 +0100

    Auto-install xdg desktop portal gtk

--- install_gui.sh
+++ install_gui.sh
@@ -99,7 +99,7 @@ paru -S --noconfirm --needed \
 
 # Wayland WM essentials
 paru -S --noconfirm --needed \
-  wl-clipboard xdg-desktop-portal qt5-wayland qt6-wayland wev wl-gammarelay-rs wdisplays
+  wl-clipboard xdg-desktop-portal xdg-desktop-portal-gtk qt5-wayland qt6-wayland wev wl-gammarelay-rs wdisplays
 
 # Utilities
 paru -S --noconfirm --needed \
