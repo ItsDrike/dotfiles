1efade836447ebed8778051583428ea280fd3a90
ItsDrike <itsdrike@protonmail.com>
2026-09-22 15:37:47 +0200

    Enable intel iommu

--- aconfmgr/hosts/orca.sh
+++ aconfmgr/hosts/orca.sh
@@ -2,3 +2,6 @@ config_root="$(dirname -- "${BASH_SOURCE[0]}")/.."
 
 source "$config_root/profiles/nvidia.sh"
 
+# Enables Intel VT-d DMA remapping.
+# (for VFIO device passthrough and DMA isolation / security hardening)
+printf '%s\n' 'intel_iommu=on' > "$(CreateFile /etc/cmdline.d/10-iommu.conf)"
