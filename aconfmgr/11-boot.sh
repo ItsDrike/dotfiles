1bd398bd4e6bdd6e1f279395e70aa7643a4691b7
ItsDrike <itsdrike@protonmail.com>
2026-09-22 18:35:33 +0200

    Auto-enable numlock from initrd

--- aconfmgr/11-boot.sh
+++ aconfmgr/11-boot.sh
@@ -5,6 +5,7 @@ AddPackage linux-headers # Headers and scripts for building modules for the Linu
 AddPackage linux-cachyos # The Linux EEVDF + LTO + AutoFDO + Propeller Cachy Sauce Kernel by CachyOS with other patches and improvements. kernel and modules
 AddPackage linux-cachyos-headers # Headers and scripts for building modules for the Linux EEVDF + LTO + AutoFDO + Propeller Cachy Sauce Kernel by CachyOS with other patches and improvements. kernel
 AddPackage linux-firmware # Firmware files for Linux - Default set
+AddPackage kbd # Console keyboard utilities; provides setleds for the initrd numlock service
 
 # Install microcode packages dynamically based on the running CPU vendor
 case "$(uname -m)" in
@@ -19,6 +20,13 @@ esac
 
 # Extend mkinitcpio hooks configuration, adding sd-encrypt
 CopyFile /etc/mkinitcpio.conf.d/10-hooks.conf
+
+# Enable numlock before the initrd's LUKS password / TPM pin prompt
+CopyFile /etc/initcpio/install/sd-numlock 755
+CopyFile /etc/systemd/system/initrd-numlock.service 644
+CreateLink \
+  /etc/systemd/system/initrd.target.wants/initrd-numlock.service \
+  ../initrd-numlock.service
 
 # Update the mkinitcpio presets to build UKIs
 CopyFile /etc/mkinitcpio.d/linux-cachyos.preset
